b6d0fa273a7dbb9c0f850d8f15e534e577a59b5b
tandaleshridhar <tandaleshridhar27@gmail.com>
2019-01-12 23:32:38 +0530

    service url upated

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -6,7 +6,7 @@ import {} from '@angular/common/http';
   providedIn: 'root'
 })
 export class CustomerService {
-  private url = 'http://localhost:8080/ai_api/customer/';
+  private url = 'http://localhost:8080/ai/php/customer/';
   constructor(private http:HttpClient) { }
 
   getCustomers(){
